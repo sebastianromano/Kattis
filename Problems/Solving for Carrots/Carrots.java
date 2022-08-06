7f5933db4b10c461a3139e59dd900c72f3ee4109
Sebastian <70913476+sebastianromano@users.noreply.github.com>
2022-08-06 03:03:42 +0200

    added more problems to the mix

---
+++ Problems/Solving for Carrots/Carrots.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Carrots {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        sc.close();
+
+        String[] hufflePuffToCarrots = s.split(" ");
+
+        System.out.println(hufflePuffToCarrots[1]);
+
+    }
+}
