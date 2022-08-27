0afdb0e52c46b0d0392ed99aafb00dc425d3cc0f
Sebastian Romano <70913476+sebastianromano@users.noreply.github.com>
2022-08-27 12:39:07 +0200

    solution to Homework problem

--- Homework/Homework.java
+++ Homework/Homework.java
@@ -1,14 +1,33 @@
 import java.util.Scanner;
 
+/*
+ * split ;
+ * [left side of ;] && [right side of ;]
+ * 
+ * split -
+ * [left side of -] && [right side of -]
+ * 
+ * Example input: 1-3;    5;7;     10-13
+ */
+
 public class Homework {
     public static void main(String[] args) {
-
-        // 1-3;5;7;10-13
-
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        input.split(";");
+        String[] inputArr = sc.nextLine().replace(" ", "").split(";");
         sc.close();
-        System.out.print(input);
+        int counter = 0;
+        for (String stringObj : inputArr) {
+            counter++;
+            if (stringObj.contains("-")) {
+                counter++;
+                String[] newString = stringObj.split("-");
+                int left = Integer.parseInt(newString[0]); // cast string to int (left)
+                int right = Integer.parseInt(newString[1]); // cast string to int (right)
+                for (int i = ++left; i < right; i++) { // get int values from one int to another int, w/o [left] [right]
+                    counter++;
+                }
+            }
+        }
+        System.out.println(counter);
     }
 }
