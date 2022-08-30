cef90654611bef75eb07c823e1fcd0abb9d2f9bd
Sebastian Romano <70913476+sebastianromano@users.noreply.github.com>
2022-08-30 19:08:52 +0200

    delete comments

--- BuildingPyramids/BuildingPyramids.java
+++ BuildingPyramids/BuildingPyramids.java
@@ -2,11 +2,6 @@ import java.util.Scanner;
 
 public class BuildingPyramids {
     public static void main(String[] args) {
-        // pyramid of various layers
-        // layer 1 = 1
-        // layer 2 = 9
-        // layer 3 = 35
-        // 
         Scanner sc = new Scanner(System.in);
         int count = 1, layer = 0, block = sc.nextInt();
         while ((block - count * count) >= 0) {
